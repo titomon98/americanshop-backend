319ee13a027a13a2770dfd2a3f1865319ade86d1
titomon98 <arturomonterroso3@gmail.com>
2025-01-21 20:28:00 -0600

    Primer commit

---
+++ src/main/java/com/product/americanshop/AmericanshopApplication.java
@@ -0,0 +1,13 @@
+package com.product.americanshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericanshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/product/americanshop/AmericanshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.americanshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericanshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
